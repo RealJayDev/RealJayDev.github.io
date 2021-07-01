3038f250d4639c57a4464cdbb46e37f4ddc69c5a
Jordan Chrimes <54643033+RealJayDev@users.noreply.github.com>
2021-07-01 12:13:43 +0100

    New Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,61 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jordan Chrimes",
+    "role": "Game Developer and Programmer",
+    "linkedinId":"Your LinkedIn Id",
+    "discordid": " JayDev#9917",
+    "robloxname": " RealJayDev",
+    "roleDescription": " I enjoy dabbling in both front and backend development to create various experiences and self-used tools.",
+    "socialLinks":[
+        {
+          "name":"github",
+          "url":"http://github.com/RealJayDev",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Twitter",
+          "url":"http://twitter.com/Real_JayDev",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently working towards creating my own game development studio. I am a largely self-taught programmer with 3 years of experience. I have started to take freelance work in order to help others achieve their game development based dreams while fulfilling my ability to do this full-time.",
+    "address":"Greater Manchester, UK",
+    "work":[
+      {
+        "Workplace":"Inkton Games",
+        "specialization":"Sole developer",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Worked on numerous games with over 800 visits"
+      },
+    ],
+    "skillsDescription":"During my time programming, I have learned these languages:",
+    "skills":[
+      {
+        "skillname":"Luau"
+      },
+      {
+        "skillname":"Typescript"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Tower Fall",
+        "description":"A Medieval tower defense game. It features level up and wave systems along with basic enemy AI.",
+        "imgurl":"images/portfolio/towerfall.jpg"
+      },
+      {
+        "name":"Noob Clicker",
+        "description":"A simplistic clicker game. It features global leaderboard and rebirth systems.",
+        "imgurl":"images/portfolio/noobclicker.png"
+      },
+    ],
+    "testimonials":[
+      /* {
+        "description":"Working with Jordan is a pleasure, work gets done quickly and to a high standard.",
+        "name":"Filosoof"
+      }, */
+    ]
+  }
+  
+  export default resumeData
