05c9ec313650390a9d068ccfba569354bfb13c5a
Jordan Chrimes <54643033+RealJayDev@users.noreply.github.com>
2021-07-01 13:06:47 +0100

    Updates

---
+++ precache-manifest.93b495fe24be00adb502d3714e1311ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4edda7170d90f9cdfa2",
+    "url": "/static/css/main.14ddf246.chunk.css"
+  },
+  {
+    "revision": "e4edda7170d90f9cdfa2",
+    "url": "/static/js/main.e4edda71.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08c91b2fdf548bf4f7b48e54d036e390",
+    "url": "/index.html"
+  }
+];
